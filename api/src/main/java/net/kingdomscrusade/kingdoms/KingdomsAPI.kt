6c4bf850546a689f8447acde470b50f5f26980ac
LittleHillMYR <woongzeyi@gmail.com>
2021-04-16 15:01:08 +0800

    ⚙️ Implemented main API class
    1. Implemented the body of the main api class*.
    2. Minor tweaks with .gitignore.
    
    * The API module is test-driven as it is a module for operations with database only. Hence there will be two new class when a class is implemented in the module, one is the class, one is the test class (located in test directory).

--- api/src/main/java/net/kingdomscrusade/kingdoms/KingdomsAPI.kt
+++ api/src/main/java/net/kingdomscrusade/kingdoms/KingdomsAPI.kt
@@ -1,4 +1,76 @@
 package net.kingdomscrusade.kingdoms
 
-class KingdomsAPI {
+import java.io.File
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+
+class KingdomsAPI(dbFilePath: String) {
+
+    private val databaseConnection: Connection
+
+    init {
+        val databaseFile = File(dbFilePath)
+        if (!databaseFile.exists()){
+            databaseConnection = DriverManager.getConnection("jdbc:sqlite:$dbFilePath")
+            createDatabaseStructure()
+        } else databaseConnection = DriverManager.getConnection("jdbc:sqlite:$dbFilePath")
+    }
+
+    private fun createDatabaseStructure() {
+        val statement = databaseConnection.createStatement()
+        statement.executeUpdate(
+            """
+                CREATE TABLE Kingdoms (
+                    kingdom_uuid STRING (36) PRIMARY KEY
+                                             NOT NULL
+                                             UNIQUE,
+                    kingdom_name STRING (12) UNIQUE
+                                             NOT NULL
+                );
+                CREATE TABLE Permissions (
+                    permission_id   INTEGER PRIMARY KEY AUTOINCREMENT
+                                            UNIQUE
+                                            NOT NULL,
+                    permission_flag STRING  NOT NULL,
+                    permission_role         NOT NULL
+                                            REFERENCES Roles (role_uuid) ON DELETE CASCADE
+                                                                         ON UPDATE CASCADE
+                );
+                CREATE TABLE Roles (
+                    role_uuid    STRING (36) PRIMARY KEY
+                                             UNIQUE
+                                             NOT NULL,
+                    role_name    STRING (12) NOT NULL,
+                    role_kingdom             REFERENCES Kingdoms ON DELETE CASCADE
+                                                                 ON UPDATE CASCADE
+                );
+                CREATE TABLE Users (
+                    user_uuid           STRING (36) UNIQUE
+                                                    NOT NULL
+                                                    PRIMARY KEY,
+                    user_name           STRING (16) UNIQUE
+                                                    NOT NULL,
+                    user_kingdom                    REFERENCES Kingdoms (kingdom_uuid) ON DELETE CASCADE
+                                                                                       ON UPDATE CASCADE
+                                                    NOT NULL,
+                    [user_kingdom:role]             REFERENCES Roles (role_uuid) ON DELETE SET NULL
+                                                                                 ON UPDATE CASCADE
+                );
+            """.trimIndent()
+        )
+    }
+
+    fun executeUpdate(update: String): Int =
+        databaseConnection.createStatement().executeUpdate(update)
+
+    fun executeQuery(query: String): ResultSet =
+        databaseConnection.createStatement().executeQuery(query)
+
+    fun closeConnection() {
+        databaseConnection.close()
+    }
+
+    fun isClosed() =
+        databaseConnection.isClosed
 }
--- api/src/test/java/net/kingdomscrusade/kingdoms/KingdomsAPITest.kt
+++ api/src/test/java/net/kingdomscrusade/kingdoms/KingdomsAPITest.kt
@@ -1,5 +1,90 @@
 package net.kingdomscrusade.kingdoms
 
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.util.*
+
 internal class KingdomsAPITest {
+
+    @BeforeEach
+    fun deleteExistingDBFile() {
+        File("D:/Code/Resources/DummyPluginData.db").delete()
+    }
+
+    @Test
+    @DisplayName("Database file creation test")
+    fun creationTest(){
+
+        /* Given */
+        val dbFilePath = "D:/Code/Resources/DummyPluginData.db"
+
+        /* When */
+        KingdomsAPI(dbFilePath)
+
+        /* Then */
+        // Checking if a db file is created
+        val dbFile = File(dbFilePath)
+        assertTrue(dbFile.exists() && dbFile.isFile)
+
+    }
+
+    @Test
+    @DisplayName("Database structure test")
+    fun structureTest(){
+        /* Given */
+        val dbFilePath = "D:/Code/Resources/DummyPluginData.db"
+
+        /* When */
+        val api = KingdomsAPI(dbFilePath)
+
+        /* Then */
+        // Checking column counts of every table
+        assertEquals( // Kingdoms
+            2,
+            api.executeQuery("SELECT * FROM Kingdoms").metaData.columnCount
+        )
+        assertEquals( // Permissions
+            3,
+            api.executeQuery("SELECT * FROM Permissions").metaData.columnCount
+        )
+        assertEquals( // Roles
+            3,
+            api.executeQuery("SELECT * FROM Roles").metaData.columnCount
+        )
+        assertEquals( // Users
+            4,
+            api.executeQuery("SELECT * FROM Users").metaData.columnCount
+        )
+
+    }
+
+    @Test
+    @DisplayName("Method \"executeUpdate\", \"executeQuery\" and \"closeConnection\" test")
+    fun executionTest(){
+        /* Given */
+        val dbFilePath = "D:/Code/Resources/DummyPluginData.db"
+        val kingdomUUID = UUID.randomUUID()
+        val kingdomName = "Midgard"
+
+        /* When */
+        val api = KingdomsAPI(dbFilePath)
+
+        /* Then */
+        // executeUpdate test
+        api.executeUpdate("INSERT INTO Kingdoms VALUES ('$kingdomUUID', '$kingdomName')")
+
+        // executeQuery test
+        val query = api.executeQuery("SELECT * FROM Kingdoms WHERE kingdom_name='$kingdomName'")
+        query.next()
+        assertEquals(kingdomUUID.toString(), query.getString("kingdom_uuid"))
+        assertEquals(kingdomName, query.getString("kingdom_name"))
+
+        // closeConnection test
+        api.closeConnection()
+        assertTrue(api.isClosed())
+    }
 
 }
